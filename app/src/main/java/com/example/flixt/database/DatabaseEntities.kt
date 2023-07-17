8282084af53a23af3592141a0d747fb07de86edd
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-07-16 23:56:44 -0400

    add database entities

---
+++ app/src/main/java/com/example/flixt/database/DatabaseEntities.kt
@@ -0,0 +1,32 @@
+package com.example.flixt.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.flixt.domain.Movie
+
+@Entity
+data class DatabaseMovie constructor(
+    @PrimaryKey
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val backdropPath: String,
+    val releaseDate: String,
+    val video: Boolean,
+    val popularity: Double,
+    val voteCount: Int,
+    val voteAverage: Double,
+    val originalTitle: String,
+    val posterPath: String,
+)
+
+fun List<DatabaseMovie>.asDomainModel(): List<Movie> {
+    return map {
+        Movie(
+            id = it.id,
+            title = it.title,
+            posterPath = it.posterPath,
+            overview = it.overview,
+        )
+    }
+}
