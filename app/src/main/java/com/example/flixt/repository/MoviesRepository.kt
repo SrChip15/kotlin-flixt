1391a380149ac98a2813708f23d94e4d4ffc5640
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-07-17 11:11:45 -0400

    add paging config

--- app/src/main/java/com/example/flixt/repository/MoviesRepository.kt
+++ app/src/main/java/com/example/flixt/repository/MoviesRepository.kt
@@ -2,9 +2,14 @@ package com.example.flixt.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.liveData
 import com.example.flixt.BuildConfig
 import com.example.flixt.data.database.MovieDatabase
 import com.example.flixt.data.database.asDomainModel
+import com.example.flixt.data.paging.MoviePagingSource
 import com.example.flixt.domain.Movie
 import com.example.flixt.network.TmdbApiService
 import com.example.flixt.network.asDatabaseModel
@@ -25,5 +30,15 @@ class MoviesRepository(
             val response = service.getMovies(BuildConfig.API_KEY, 1)
             database.movieDao.insertAll(*response.movies.asDatabaseModel())
         }
+    }
+
+    fun getMoviesStream(page: Int): LiveData<PagingData<Movie>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { MoviePagingSource(service) }
+        ).liveData
     }
 }
