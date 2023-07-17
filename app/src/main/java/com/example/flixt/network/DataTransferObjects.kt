82674df5117f444de89992ae76f7c6a3a4d64196
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-07-16 23:59:06 -0400

    add dao

---
+++ app/src/main/java/com/example/flixt/network/DataTransferObjects.kt
@@ -0,0 +1,46 @@
+package com.example.flixt.network
+
+import android.os.Parcelable
+import com.example.flixt.database.DatabaseMovie
+import com.squareup.moshi.Json
+import kotlinx.parcelize.Parcelize
+
+data class DiscoverResponse(
+    val results: List<NetworkMovie>,
+    val page: Int,
+    @Json(name = "total_results") val totalResults: Int,
+    @Json(name = "total_pages") val totalPages: Int,
+)
+
+@Parcelize
+data class NetworkMovie(
+    val id: Int,
+    @Json(name = "vote_count") val voteCount: Int,
+    val video: Boolean,
+    @Json(name = "vote_average") val voteAverage: Double,
+    val title: String,
+    val popularity: Double,
+    @Json(name = "poster_path") val posterPath: String,
+    @Json(name = "original_title") val originalTitle: String,
+    @Json(name = "backdrop_path") val backdropPath: String,
+    val overview: String,
+    @Json(name = "release_date") val releaseDate: String,
+) : Parcelable
+
+fun List<NetworkMovie>.asDatabaseModel(): Array<DatabaseMovie> {
+    return map {
+        DatabaseMovie(
+            id = it.id,
+            title = it.title,
+            overview = it.overview,
+            backdropPath = it.backdropPath,
+            releaseDate = it.releaseDate,
+            video = it.video,
+            popularity = it.popularity,
+            voteCount = it.voteCount,
+            voteAverage = it.voteAverage,
+            originalTitle = it.originalTitle,
+            posterPath = it.posterPath,
+        )
+    }.toTypedArray()
+}
