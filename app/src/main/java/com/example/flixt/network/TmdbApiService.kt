2e22d3e4af8017e88137f77b3cd62b59606e8d2d
SrChip15 <12288245+SrChip15@users.noreply.github.com>
2023-07-23 14:54:46 -0400

    fit whole movie poster

--- app/src/main/java/com/example/flixt/network/TmdbApiService.kt
+++ app/src/main/java/com/example/flixt/network/TmdbApiService.kt
@@ -27,7 +27,7 @@ interface TmdbApiService {
     companion object {
         const val BASE_URL = "https://api.themoviedb.org/3/"
         const val POSTER_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/"
-        const val POSTER_IMAGE_SIZE = "w780"
+        const val POSTER_IMAGE_SIZE = "w300"
     }
 }
 
